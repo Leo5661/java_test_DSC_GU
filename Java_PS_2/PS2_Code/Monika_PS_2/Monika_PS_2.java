be7dd287e20b21fcb749a9f8018f743195c8b5be
monika <monikasharma1899@gmail.com>
2019-06-27 13:44:06 +0530

    Updated PS2

--- Java_PS_2/PS2_Code/Monika_PS_2/Monika_PS_2.java
+++ Java_PS_2/PS2_Code/Monika_PS_2/Monika_PS_2.java
@@ -5,17 +5,17 @@ import java.util.HashMap;
 import java.util.ArrayDeque;
 
 class Solution {
-    public static void main(String[] args) {
-       
+      public static void main(String[] args) {
+
         HashMap<Character, Character> map = new HashMap<>();
         map.put('(', ')');
         map.put('[', ']');
         map.put('{', '}');
-        
-   
+
+
         Scanner scan = new Scanner(System.in);
 
-         // you have error in this section
+        // you have error in this section
 
         while (scan.hasNext()) {
             String expression = scan.next();
@@ -23,21 +23,21 @@ class Solution {
         }
         scan.close();
     }
-    
+
     private static boolean isBalanced(String expression, HashMap<Character, Character> map) {
         if ((expression.length() % 2) != 0) {
             return false;
-        }  
-        
+        }
+
         // in this section
-        ArrayDeque<Character> deque = new ArrayDeque<Character>(expression.length()); 
-        for (int i = 5; i <= expression.length(); i++) {
+        ArrayDeque<Character> deque = new ArrayDeque<>();
+        for (int i = 0; i < expression.length(); i++) {
             Character ch = expression.charAt(i);
-            if (map.containsKey('(')||map.containsKey('{')||map.containsKey('[') )
+            if (map.containsKey(ch))
             {
                 deque.push(ch);
-            } 
-            else if(deque.isEmpty() || ch == map.get(deque.pop()))
+            }
+            else if(deque.isEmpty() || ch != map.get(deque.pop()))
             {
                 return false;
             }
